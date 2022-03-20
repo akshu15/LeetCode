2d34d55ecf4aaed6b3dc509e9f787468191f1c5a
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-03-20 15:13:26 -0400

    Time: 56 ms (90.64%), Space: 18.1 MB (11.45%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -7,43 +7,16 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        
-        #0<=pos<=length(list)
-        
-        #find the length
-        
         if head==None:
             return
         itr=ListNode()
         itr=head
         hashMap={}
         
-        while itr.next: #iterate through the linked list till reaches end
+        while itr.next: #iterate through the linked list till we reache the end
             
             if itr not in hashMap:
                 hashMap[itr]=itr.val
                 itr=itr.next
             else:
                 return True
-                
-        
-        
-#         count=0
-        
-#         pos1=pos
-        
-#         if count==pos1:  #at head is the pos
-#             return 
-        
-#         elif pos==-1:
-#             return 
-        
-#         print(itr)
-        
-#         while itr.next: #iterate through the linked list till reaches end
-            
-#             if count==pos:
-#                 break
-#             else:
-#                 itr=itr.next
-#                 count+=1
