ee66761d2fc2e63e779bfbe9927b63b55befdc97
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-03-21 18:44:23 -0400

    Time: 77 ms (9.54%), Space: 14.1 MB (14.35%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,30 +6,57 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        # print(list1)
-        dummy = ListNode()
-        newlist = dummy
-        while(list1!=None and list2 !=None): #until any of the list reaches the end
-            
-            if (list1.val<=list2.val):
-                newlist.next=list1
-                # print(list1)
-                newlist = newlist.next
+        if list1==None and list2==None:
+            return
+        elif list1==None:
+            return list2
+        elif list2==None:
+            return list1
+        
+        else:
+            if list1.val<=list2.val:
+                node=ListNode(list1.val)
                 list1=list1.next
             else:
-                newlist.next=list2
-                newlist = newlist.next
+                node=ListNode(list2.val)
                 list2=list2.next
-                # print(newlist)
                 
+            head=node
+            itr=head
+            
+            
+            while list1!=None and list2!=None:
+                # print(head)
                 
+                if list1.val<=list2.val:
+                    node=ListNode(list1.val)
+                    list1=list1.next
+                else:
+                    node=ListNode(list2.val)
+                    list2=list2.next
+                    
+                if head.next==None:
+                    head.next=node
+                else:
+                    itr.next=node
+                itr=itr.next
+                    
             
-        
-        if list1==None:
-            newlist.next=list2
+            if list1!=None and list2==None:
+                while list1!=None:
+                    itr.next = list1
+                    list1 = list1.next
+                    itr = itr.next
             
-        elif list2==None:
-            newlist.next=list1
-        
-        return dummy.next
+            if list2!=None and list1==None:
+                while list2!=None:
+                    itr.next = list2
+                    list2 = list2.next
+                    itr = itr.next
+                    
+                
+            print(head)
+            return head
+            # print(node)
+    
         
