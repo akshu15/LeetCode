74320ee6fa9da05a0215cf1b2d6789fc85474562
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-03-23 22:28:23 -0400

    Time: 115 ms (78.04%), Space: 18.3 MB (40.31%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,24 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        if len(strs)==1:
+            return [strs]
+        
         hashMap={}
         
-        # nList=[]
-        if len(strs)>=1:
+        for i in range(len(strs)):
             
-            for i in range(len(strs)):
-
-
-                my=list(strs[i])
-                my.sort()
-                # print(my)
-                if tuple(my) in hashMap:
-                    hashMap[tuple(my)].append(strs[i])
-                else:
-                    hashMap[tuple(my)]=[strs[i]]
-
-                # for i in range(hashMap[i])
-
-            return(hashMap.values())
-        else:
-            return
+            x=list(strs[i])
+            x.sort()
+            
+            if tuple(x) in hashMap:
+                hashMap[tuple(x)].append(strs[i])
+            else:
+                hashMap[tuple(x)]=[strs[i]]
+            
+        return(hashMap.values())
+            
+            
+        
