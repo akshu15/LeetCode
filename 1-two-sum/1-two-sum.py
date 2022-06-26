f110fc1ec0da524ed90e42d529fcbc39739412db
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-06-26 11:28:33 -0400

    Time: 98 ms (59.08%), Space: 15.2 MB (49.67%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,25 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        hashMap={}
-        
-        
-        #nums[i]=2   second_num=target-nums[i]=9-2=7   is it in hash?
-        #No:-    hashMap={ 2: 0 }
-        #nums[i]=7. second_num=target-nums[i]=9-7=2   is it in hash?
-        #Yes:-   return (i, hashMap[second_num])
-        
-        
+        hashT = {}
         
         for i in range(len(nums)):
-            second_num=target-nums[i]       
+            newTarget = target  - nums[i] 
             
-            if second_num in hashMap:
-                return([i,hashMap[second_num]])
+            if newTarget not in hashT:
+                hashT[nums[i]] = i
             else:
-                hashMap[nums[i]] = i 
-            
-        
-        
-        
-        
+                return (hashT[newTarget], i)
