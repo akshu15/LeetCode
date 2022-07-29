69de8830674d7872473ef19fc5bc06e867d595b8
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-07-28 20:26:48 -0400

    Time: 34 ms (94.92%), Space: 13.8 MB (96.82%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -6,12 +6,10 @@ class Solution:
         while total>4:
             total=0
             for i in range(len(str1)):
-                # print(str1[i])
                 el=str1[i]
                 total+=int(el)**2
             str1=str(total)
             
-        # if total<4:
             
         if(str1[0]=='1'):
             return True
