13b39ae5571f6cdaa87c6dd249c7f2c37b0b027e
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-08-02 18:34:04 -0400

    Time: 834 ms (72.73%), Space: 19.3 MB (25.37%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,10 +1,18 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) < sum(cost): 
+        # base case
+        if sum(gas) - sum(cost) < 0:
             return -1
-        n, start, agg = len(gas), 0, 0
-        for i in range(n):
-            agg += gas[i] - cost[i]
-            if agg < 0:
-                start, agg = i+1, 0
-        return start
+
+        gas_tank = 0  # gas available in car till now
+        start_index = 0  # Consider first gas station as starting point
+
+        for i in range(len(gas)):
+
+            gas_tank += gas[i] - cost[i]
+
+            if gas_tank < 0:  # the car has deficit of petrol
+                start_index = i+1  # change the starting point
+                gas_tank = 0  # make the current gas to 0, as we will be starting again from next station
+
+        return start_index
