7f75fce9304e21399cf66d7a542ca97fbc57b702
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-08-25 21:49:26 -0400

    Time: 60 ms (87.55%), Space: 14.2 MB (20.43%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -7,18 +7,13 @@ class Solution:
                 hashMap[letter] += 1
             else:
                 hashMap[letter] = 1
-        print("magazine",magazine)
-        print("ransomNote",ransomNote)
-        print(hashMap)
         
         for letter in ransomNote:
             if letter in hashMap:
                 if hashMap[letter]>=1:
                     hashMap[letter]-=1
-                    print("ff",hashMap)
                 else:
                     return False
             else:
-                print("l")
                 return False
         return True
