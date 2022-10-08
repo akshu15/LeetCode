c95121f0358bd476d67484e36f48e758e6d1e10e
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-10-07 23:55:14 -0400

    Time: 99 ms (18.78%), Space: 16 MB (99.80%) - LeetHub

---
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return []
+        
+        if not root.left and not root.right:
+            return root.val
+        q = deque([root])
+        BFS=[]
+        n_arr=[]
+        
+        while q:
+            
+            level = []
+            n=[]
+            for i in range(len(q)):
+                node = q.popleft()
+                
+                if node:
+                    if node.left: 
+                        q.append(node.left)
+                        
+                    if node.right:
+                        q.append(node.right) 
+                        
+                if not node.left: 
+                    n.append(None) 
+                else: 
+                    n.append(node.left.val)
+                    
+                
+                if not node.right: 
+                    n.append(None)
+                else: 
+                    n.append(node.right.val)
+                # print(n)
+                val = node.val
+                level.append(val) 
+                
+            BFS.append(level)
+            n_arr.append(n)
+        
+        lastLevel=n_arr[-2]
+        for i in range(len(lastLevel)):
+            if lastLevel[i] is not None:
+                return(lastLevel[i])
