5b98bb1822037d27754e86161de1a42e1651a335
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-11-11 21:17:14 -0500

    Time: 74 ms (29.32%), Space: 13.8 MB (57.52%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -18,13 +18,10 @@ class Solution:
             count+=1
             i=0
             while i < len(nums):
-                # print(nums[i] , mini)
                 if nums[i] - mini == 0:
-                    # print("ji")
                     nums.pop(i)
                 else:
                     nums[i] = nums[i] - mini
                     i+=1
-                # print(nums)
         return(count)
             
