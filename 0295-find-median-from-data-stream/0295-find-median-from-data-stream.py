d9b8c1028d95bb598fe09ce0b6419b4c9ad6ae17
akshu15 <52970601+akshu15@users.noreply.github.com>
2022-11-12 13:21:03 -0500

    Time: 7939 ms (5.00%), Space: 35.9 MB (66.11%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -12,17 +12,11 @@ class MedianFinder:
         self.stack.sort()
         length = len(self.stack)
         if length%2==0:      #Even
-            # print(self.stack)
             a = math.floor((length-1)/2) 
             b = math.ceil((length-1)/2)
-            # # print(length/2)
-            # print(a, b)
             median = (self.stack[a]+self.stack[b])/2
-            # print(median)
             return median
         else:
-            # print("here",self.stack)
-            # print(self.stack[length//2])
             return self.stack[length//2]
 
 
